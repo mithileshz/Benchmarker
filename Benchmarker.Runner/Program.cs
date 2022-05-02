0c999a7a693c6acd6154617847cfd4712d55ee4c
Mithilesh Zavar <mithileshz@hotmail.co.uk>
2022-05-02 22:26:49 +0100

    Changes

--- Benchmarker.Runner/Program.cs
+++ Benchmarker.Runner/Program.cs
@@ -1,9 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using BenchmarkDotNet.Running;
+using Benchmarker.Benchmarks;
 
-using BenchmarkDotNet.Running;
-using System;
-//using Benchmarks.Benchmarks;
-
-//BenchmarkRunner.Run<InitialisingEmptyArray>();
-
-Console.Write("Hello world");
+BenchmarkRunner.Run<InitialisingEmptyArray>();
